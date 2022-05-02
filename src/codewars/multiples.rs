33c146a6da37141b14f9f49c28e3ded05bdda6d4
Tom Scott <tscott@stratanetworks.com>
2022-05-02 14:06:52 -0600

    found a better solution

--- src/codewars/multiples.rs
+++ src/codewars/multiples.rs
@@ -1,20 +1,16 @@
-pub fn solution(num: i32) -> i32 {
-    let mut num_list = Vec::new();
+pub fn my_solution(num: i32) -> i32 {
+    let mut total = 0;
     for number in 0..num {
-        if number % 5 == 0 && number % 3 == 0 {
-            num_list.push(number);
-        } else if number % 5 == 0 {
-            num_list.push(number);
+        if number % 5 == 0 {
+            total += number;
         } else if number % 3 == 0 {
-            num_list.push(number);
+            total += number;
         }
     }
-
-    let mut total = 0;
-
-    for another in num_list.iter() {
-        total += another;
-    }
-
+    println!("{total}");
     return total;
+}
+
+pub fn clever_solution(num: i32) -> i32 {
+    (1..num).filter(|x| x % 3 == 0 || x % 5 == 0).sum()
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ mod first;
 fn main() {
     first::formatted_print::main();
 
-    assert_eq!(codewars::multiples::solution(10), 23);
-    assert_eq!(codewars::multiples::solution(11), 33);
-    assert_eq!(codewars::multiples::solution(6), 8);
+    assert_eq!(codewars::multiples::my_solution(10), 23);
+    assert_eq!(codewars::multiples::my_solution(11), 33);
+    assert_eq!(codewars::multiples::my_solution(33), 225);
+    assert_eq!(codewars::multiples::clever_solution(10500), 25719750);
+    assert_eq!(codewars::multiples::clever_solution(50), 543);
 }
